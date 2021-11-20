13f362aa7d056ccf832361d672765a2beb761615
Aleksandr Blinov <jiuteoh@gmail.com>
2021-11-21 01:16:07 +0300

    style(index.js): changed import bracers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './styles/style.scss';
 
-import {renderMainForm} from "./pages/main/main";
-import {renderError} from "./pages/error/error";
-import {renderAuthForm} from "./pages/auth/auth";
-import {renderChat} from "./pages/chat/chat";
-import {renderUserProfileForm} from "./pages/user-profile/user-profile";
+import {renderMainForm} from './pages/main/main';
+import {renderError} from './pages/error/error';
+import {renderAuthForm} from './pages/auth/auth';
+import {renderChat} from './pages/chat/chat';
+import {renderUserProfileForm} from './pages/user-profile/user-profile';
 
 const root = document.querySelector(".root");
 const path = window.location.pathname;
